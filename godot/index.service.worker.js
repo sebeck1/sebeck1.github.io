7adc4dbfa930f09edff8cd7a404cc364963bdaec
Sebastian Beck <sebastian.beck1993@gmail.com>
2023-06-28 19:35:02 +0200

    first

--- godot/index.service.worker.js
+++ godot/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1687973252|5289030149";
+const CACHE_VERSION = "1687973669|5706035607";
 const CACHE_PREFIX = "Run_the_Tower-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
